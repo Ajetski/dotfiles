5d9ef47cabcd0e74ed1e2d2d023f6d85278636a3
Adam Jeniski <adam.jeniski@ibm.com>
2022-11-18 16:45:04 -0500

    change themes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -171,62 +171,87 @@ require('nvim_comment').setup({
 require 'hop'.setup {}
 
 -- setup lualine
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.blue },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.violet } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
-}
+-- local colors = {
+--   blue   = '#80a0ff',
+--   cyan   = '#79dac8',
+--   black  = '#080808',
+--   white  = '#c6c6c6',
+--   red    = '#ff5189',
+--   violet = '#d183e8',
+--   grey   = '#303030',
+-- }
+--
+-- local bubbles_theme = {
+--   normal = {
+--     a = { fg = colors.black, bg = colors.blue },
+--     b = { fg = colors.white, bg = colors.grey },
+--     c = { fg = colors.black, bg = colors.black },
+--   },
+--
+--   insert = { a = { fg = colors.black, bg = colors.violet } },
+--   visual = { a = { fg = colors.black, bg = colors.cyan } },
+--   replace = { a = { fg = colors.black, bg = colors.red } },
+--
+--   inactive = {
+--     a = { fg = colors.white, bg = colors.black },
+--     b = { fg = colors.white, bg = colors.black },
+--     c = { fg = colors.black, bg = colors.black },
+--   },
+-- }
 
 require('lualine').setup {
   options = {
-    theme = bubbles_theme,
+    theme = 'modus-vivendi', -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
     component_separators = '|',
     section_separators = { left = '', right = '' },
   },
   sections = {
     lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+      -- { 'mode', separator = { left = '' }, right_padding = 2 },
+      'mode'
     },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
+    lualine_b = {{ 'filename', path = 1 }, 'branch', 'diff', 'diagnostics' },
+    lualine_c = {'searchcount'},
     lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
+    lualine_y = {'fileformat', { 'filetype', colored = true, icon_only = true, padding = { left = 1, right = 2 }}},
     lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
+      -- { 'location', separator = { right = '' }, left_padding = 2 },
+      'progress'
     },
   },
   inactive_sections = {
-    lualine_a = { 'filename' },
+    lualine_a = { {'filename', path = 1} },
     lualine_b = {},
     lualine_c = {},
     lualine_x = {},
     lualine_y = {},
-    lualine_z = { 'location' },
+    lualine_z = {'progress'},
   },
-  tabline = {},
-  extensions = {},
+  tabline = {
+    lualine_a = {'buffers'},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {'tabs'},
+  },
+  winbar = {
+  lualine_a = {},
+  lualine_b = {},
+  lualine_c = {},
+  lualine_x = {},
+  lualine_y = {},
+  lualine_z = {}
+  },
+  inactive_winbar = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  extensions = {'quickfix'},
 }
 
 -- setup comments
