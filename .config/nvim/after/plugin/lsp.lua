8c9f9941605e031b95c0a212c88ddde3a16aee6c
Adam Jeniski <adam.jeniski@ibm.com>
2023-02-07 22:39:36 -0500

    kill eslint

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -49,15 +49,13 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(_client, bufnr)
+lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
-    --[[
     if client.name == "eslint" then
         vim.cmd.LspStop('eslint')
         return
     end
-    ]]--
 
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
