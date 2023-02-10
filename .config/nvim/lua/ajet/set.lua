59e82b9f410758c7812047629ddcb9ed46027067
Adam Jeniski <adam.jeniski@ibm.com>
2023-02-10 12:58:27 -0500

    use 2 space tabs (for sfi proj; subject to revert/change)

--- .config/nvim/lua/ajet/set.lua
+++ .config/nvim/lua/ajet/set.lua
@@ -3,12 +3,12 @@ vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkwait2000-b
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-vim.opt.foldlevel = 20
+vim.opt.foldlevel = 3
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
