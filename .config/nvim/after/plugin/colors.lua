7fd777799987d2a315019755a74b0e0704e325d4
Adam Jeniski <adam.jeniski@flashtalking.com>
2023-08-01 09:13:11 -0400

    add light theme toggle

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,6 +1,10 @@
 function ColorMyPencils(color)
-	color = color or "catppuccin"
+	color = color or "catppuccin-frappe"
 	vim.cmd.colorscheme(color)
 end
 
 ColorMyPencils()
+
+vim.keymap.set('n', '<leader>tl', ':TransparentDisable<cr>:colorscheme catppuccin-latte<cr>')
+vim.keymap.set('n', '<leader>td', ':TransparentEnable<cr>:colorscheme catppuccin-frappe<cr>')
+
